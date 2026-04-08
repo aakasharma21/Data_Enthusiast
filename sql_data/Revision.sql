c5f405ddfe1fe45d3d6429d88a6cf4d68ff62a7d
Aakash Sharma <aakashs0101@gmail.com>
2026-04-08 10:38:47 +0530

    Auto update: 2026-04-08 10:38:47

--- sql_data/Revision.sql
+++ sql_data/Revision.sql
@@ -14,3 +14,45 @@ VALUES(1005,5,'01-04-2026',40),
 		(1006,6,'02-04-2026',50),	
 		(1007,7,'03-04-2026',60)
 
+
+--DELETE FROM Employee
+--WHERE id IN(1,2,3,4,5,6)
+
+/*UPDATE Employee
+SET id=1
+WHERE first_name='Aakash Sharma'
+
+UPDATE Employee
+SET id=2
+WHERE first_name='Laddu'
+
+UPDATE Employee
+SET id=3
+WHERE first_name='Banke'
+
+UPDATE Employee
+SET id=4
+WHERE first_name='Radhe'
+
+UPDATE Employee
+SET id=5
+WHERE first_name='Shaali'
+
+UPDATE Employee
+SET id=6
+WHERE first_name='Krishu'
+*/
+
+
+INSERT INTO Employee(id,first_name)
+VALUES(1,'Aakash Sharma'), (2,'Laddu'), (3,'Banke'), (4,'Radhe'), (5,'Shaali'), (6,'Krishu');
+		
+SELECT * FROM Players
+
+SELECT * FROM Employee AS E
+FULL JOIN customers AS C
+ON E.id=C.id
+
+SELECT * FROM Employee AS E
+LEFT JOIN customers AS C
+ON E.id=C.id
